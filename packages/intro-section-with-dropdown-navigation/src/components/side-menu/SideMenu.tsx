d64bc11f43c146290f4a4cc05fff38dc6bde0da2
Samuel Silva <samuelsilvawb@gmail.com>
2022-10-20 22:23:35 +0100

    feat: Close menu if escape is pressed

--- packages/intro-section-with-dropdown-navigation/src/components/side-menu/SideMenu.tsx
+++ packages/intro-section-with-dropdown-navigation/src/components/side-menu/SideMenu.tsx
@@ -1,12 +1,19 @@
 import { Button } from "components/button";
 import { ReactComponent as CloseMenuIcon } from "components/icons/icon-close-menu.svg";
 import { useMenu } from "state/menu/MenuProvider";
+import { useEventListener } from "usehooks-ts";
 import { Aside, CloseButtonWrapperDiv } from "./SideMenu.styles";
 
 type SideMenuProps = React.HTMLAttributes<HTMLElement>;
 
 export function SideMenu(props: SideMenuProps) {
   const { close } = useMenu();
+
+  useEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      close();
+    }
+  });
 
   return (
     <Aside {...props}>
