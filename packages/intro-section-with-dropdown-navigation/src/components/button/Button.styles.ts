0fffe3d25ee342bd4b11c3885922c89ea0bdbc6f
Samuel Silva <samuelsilvawb@gmail.com>
2022-10-27 09:01:15 +0100

    feat: Update button to include new variants

--- packages/intro-section-with-dropdown-navigation/src/components/button/Button.styles.ts
+++ packages/intro-section-with-dropdown-navigation/src/components/button/Button.styles.ts
@@ -1,11 +1,18 @@
 import { COLORS } from "components/styles.config";
 import styled, { css } from "styled-components";
 
-export const StyledButton = styled.button<{ variation: string }>`
+export const StyledButton = styled.button<{
+  variation: string;
+  full?: boolean;
+  align: string;
+}>`
   background: none;
   border: none;
   cursor: pointer;
   font-family: inherit;
+  padding: 14px 24px;
+  width: ${({ full }) => (full ? "100%" : "auto")};
+  text-align: ${({ align }) => align};
 
   ${({ variation }) => {
     if (variation === "primary") {
@@ -13,8 +20,24 @@ export const StyledButton = styled.button<{ variation: string }>`
         font-size: 16px;
         background-color: ${COLORS.ALMOST_BLACK};
         color: ${COLORS.ALMOST_WHITE};
-        padding: 14px 24px;
         border-radius: 13px;
+      `;
+    }
+
+    if (variation === "secondary") {
+      return css`
+        font-size: 16px;
+        background-color: ${COLORS.ALMOST_WHITE};
+        border: 2px solid ${COLORS.MEDIUM_GRAY};
+        color: ${COLORS.MEDIUM_GRAY};
+        border-radius: 13px;
+      `;
+    }
+
+    if (variation === "link") {
+      return css`
+        font-size: 16px;
+        color: ${COLORS.MEDIUM_GRAY};
       `;
     }
   }};
--- packages/intro-section-with-dropdown-navigation/src/components/button/Button.tsx
+++ packages/intro-section-with-dropdown-navigation/src/components/button/Button.tsx
@@ -3,16 +3,25 @@ import { StyledButton } from "./Button.styles";
 
 type ButtonProps = {
   variation: "basic" | "primary" | "secondary" | "link";
+  full?: boolean;
+  align?: "start" | "end" | "center";
   children: ReactNode;
 } & React.HTMLAttributes<HTMLButtonElement>;
 
 export function Button({
   children,
   variation,
+  full = false,
+  align = "start",
   ...remainingProps
 }: ButtonProps) {
   return (
-    <StyledButton variation={variation} {...remainingProps}>
+    <StyledButton
+      variation={variation}
+      full={full}
+      align={align}
+      {...remainingProps}
+    >
       {children}
     </StyledButton>
   );
