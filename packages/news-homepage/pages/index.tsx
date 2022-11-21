5eb96bd54384add7efa09334909e9dc0124347ac
Samuel Silva <samuelsilvawb@gmail.com>
2022-11-21 23:02:28 +0000

    chore: Create empty project for news homepage and remove lock file in favor of root lock

---
+++ packages/news-homepage/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <Head>
+      <title>News</title>
+      <meta name="description" content="Worldwide news curated for you" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
