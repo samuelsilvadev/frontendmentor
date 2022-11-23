a31b8c83fc70f9667d01683a12ed50ed1b7dc63b
Samuel Silva <samuelsilvawb@gmail.com>
2022-11-23 23:30:09 +0000

    fix: Set asset prefix to load javascript correctly

--- packages/news-homepage/next.config.js
+++ packages/news-homepage/next.config.js
@@ -1,7 +1,10 @@
+const isProduction = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  assetPrefix: isProduction ? "/news-homepage" : undefined,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
