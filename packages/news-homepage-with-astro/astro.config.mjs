16e2ff15aa200ed1b07f250260c2fc1136f6eab7
Samuel Silva <samuelsilvawb@gmail.com>
2023-04-28 15:37:10 +0100

    fix: configure astro to have different base for assets when building

--- packages/news-homepage-with-astro/astro.config.mjs
+++ packages/news-homepage-with-astro/astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 
-// https://astro.build/config
+const isProduction = process.env.MODE === "production";
+
+/**
+ * @see https://astro.build/config
+ */
 export default defineConfig({
-  integrations: [image()]
+  integrations: [image()],
+  base: isProduction ? "/news-homepage-with-astro" : "/",
 });
