2e3f5657b6ec6febb04bdc8617441a92a6478804
Samuel Silva <samuelsilvawb@gmail.com>
2023-04-30 19:07:54 +0100

    feat(news-homepage-with-astro): dynamically load main new

--- packages/news-homepage-with-astro/src/types/ContentResponse.ts
+++ packages/news-homepage-with-astro/src/types/ContentResponse.ts
@@ -1,9 +1,11 @@
 import type { Menu } from "./Menu";
 import type { PostRecommendation } from "./PostRecommendation";
+import type { PublishedMainNew } from "./PublishedMainNew";
 import type { RelatedNew } from "./RelatedNew";
 
 export type ContentResponse = {
   menus?: Menu[];
   "posts-recommendations"?: PostRecommendation[];
   "related-posts"?: RelatedNew[];
+  "main-new"?: PublishedMainNew;
 };
